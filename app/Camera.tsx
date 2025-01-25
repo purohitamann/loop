625c8f0949f34387dc99e54596c8602487032c1e
Aman Purohit <purohiam@sheridancollege.ca>
2025-01-25 10:50:12 -0500

    image picker and video preview

--- app/Camera.tsx
+++ app/Camera.tsx
@@ -1,12 +1,16 @@
 import { Ionicons } from '@expo/vector-icons';
 import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Button, Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import React from 'react';
 import { supabase } from '@/utils/supabase';
 import { useAuth } from '@/providers/AuthProvider';
 import { useRouter } from 'expo-router';
+import * as ImagePicker from 'expo-image-picker';
+import { useEvent } from 'expo';
+import { useVideoPlayer, VideoView } from 'expo-video';
+
 export default function App() {
     const [facing, setFacing] = useState<CameraType>('back');
     const [permission, requestPermission] = useCameraPermissions();
@@ -15,6 +19,16 @@ export default function App() {
     const [video, setVideo] = React.useState<string | null>(null);
     const { user } = useAuth();
     const router = useRouter();
+    const source = {
+        uri: video, resizeMode: 'contain'
+    };
+    const player = useVideoPlayer(source.uri, playerInstance => {
+        playerInstance.loop = true;
+        playerInstance.play();
+    });
+
+    const { isPlaying } = useEvent(player, 'playingChange', { isPlaying: player.playing });
+
     if (!permission) {
         // Camera permissions are still loading.
         return <View />;
@@ -75,46 +89,101 @@ export default function App() {
             console.error(errorUser)
         }
         router.back();
-        // if (video) {
-        //     await cameraRef.current?.saveVideoToLibraryAsync(video);
-        // }
+
     };
-
+    const pickImage = async () => {
+        // No permissions request is necessary for launching the image library
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ['images', 'videos'],
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 0.1,
+        });
+        console.log("starting to pick image")
+        setVideo(result.assets[0].uri);
+        console.log(result.assets[0].uri)
+        console.log("picked image")
+    }
     return (
-        <View style={styles.container}>
-            <CameraView mode='video' ref={cameraRef} style={styles.camera} facing={facing}>
+        <View style={{ flex: 1, backgroundColor: 'black', width: '100%', height: '100%' }}>
+            {video ? <View style={{ flex: 1, backgroundColor: 'black' }}>
+                <TouchableOpacity
+                    onPress={() => {
+                        isPlaying ? player.pause() : player.play();
+                    }}
+                    style={{
+                        flex: 1,
+                        width: Dimensions.get('window').width,
+                        height: Dimensions.get('window').height,
+                    }}
+                >
+                    {/* Video View to occupy full screen */}
+                    <VideoView
+                        player={player}
+                        style={{
+                            flex: 1,
+                            width: '100%',
+                            height: '100%',
+                        }}
+                        allowsFullscreen
+                        showsTimecodes
+                        resizeMode="cover" // Ensures the video fills the screen
+                    />
+                </TouchableOpacity>
 
-                <View style={styles.buttonContainer}>
-                    {/* <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-                        <Text style={styles.text}>Flip Camera</Text>
-                    </TouchableOpacity> */}
-                    <View className='flex-1 justify-end '>
-                        <View className='flex-row justify-around'>
-                            {video ? (
-                                <TouchableOpacity onPress={saveVideo} className=''>
-                                    <Ionicons name="checkmark-circle" size={49} color="white" />
-                                </TouchableOpacity>
-                            ) : (
-                                !recording ? (
-                                    <TouchableOpacity onPress={recordVideo} className=''>
-                                        <Ionicons name="videocam" size={49} color="white" />
+
+                <TouchableOpacity
+                    onPress={saveVideo}
+                    style={{
+                        position: 'absolute',
+                        bottom: 50,
+                        left: 158,
+
+                        zIndex: 30,
+                    }}
+                >
+                    <Ionicons name="checkmark-circle" size={90} color="white" />
+                </TouchableOpacity>
+            </View>
+
+                : <View className='flex-1 bg-black'>
+                    <CameraView mode='video' ref={cameraRef} style={styles.camera} facing={facing}>
+
+                        <View style={styles.buttonContainer}>
+
+                            <View className='flex-1 justify-end '>
+                                <View className='flex-row justify-around'>
+                                    <TouchableOpacity onPress={pickImage} className=''>
+                                        <Ionicons name="aperture" size={49} color="white" />
                                     </TouchableOpacity>
-                                ) : (
-                                    <TouchableOpacity onPress={recordVideo} className=''>
-                                        <Ionicons name="stop-circle" size={49} color="red" />
+                                    {video ? (
+                                        <TouchableOpacity onPress={saveVideo} className=''>
+                                            <Ionicons name="checkmark-circle" size={49} color="green" />
+                                        </TouchableOpacity>
+                                    ) : (
+                                        !recording ? (
+                                            <TouchableOpacity onPress={recordVideo} className=''>
+                                                <Ionicons name="videocam" size={49} color="white" />
+                                            </TouchableOpacity>
+                                        ) : (
+                                            <TouchableOpacity onPress={recordVideo} className=''>
+                                                <Ionicons name="stop-circle" size={49} color="red" />
+                                            </TouchableOpacity>
+                                        )
+                                    )}
+                                    <TouchableOpacity onPress={toggleCameraFacing} className=''>
+                                        <Ionicons name="camera-reverse-sharp" size={49} color="white" />
                                     </TouchableOpacity>
-                                )
-                            )}
-                            <TouchableOpacity onPress={toggleCameraFacing} className=''>
-                                <Ionicons name="camera-reverse-sharp" size={49} color="white" />
-                            </TouchableOpacity>
+                                </View>
+
+                            </View>
                         </View>
-
-                    </View>
-                </View>
-            </CameraView>
+                    </CameraView>
+                </View>}
         </View>
+
     );
+
 }
 
 const styles = StyleSheet.create({
